0b18de929dd65dcea57390c05b5c2ee981892feb
mike-gangl <michael.e.gangl@jpl.nasa.gov>
2025-07-30 11:37:26 -0700

    fixed env variable usage of SAR_TASK_ID

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ ls -l
 conda run -n sar_eddy_env python src/main.py --config config/inference.yaml
 
 tar -czvf outputs.tar.gz -C output .
-conda run -n sar_eddy_env aws s3 cp outputs.tar.gz s3://$OUTPUT_BUCKET_NAME/sar_eddy/$SAR_TASK_ID_outputs.tar.gz
+conda run -n sar_eddy_env aws s3 cp outputs.tar.gz s3://$OUTPUT_BUCKET_NAME/sar_eddy/${SAR_TASK_ID}_outputs.tar.gz
