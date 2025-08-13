c7dccf2374c9ba4f25819b242a4c800ab3c5fde9
mike-gangl <michael.e.gangl@jpl.nasa.gov>
2025-08-13 13:21:10 -0700

    list some AWS info

--- run.sh
+++ run.sh
@@ -3,7 +3,9 @@ printenv
 echo "$PWD"
 ls -l
 
+
+conda run -n sar_eddy_env aws configure list
 conda run -n sar_eddy_env python src/main.py --config config/inference.yaml
 
 tar -czvf outputs.tar.gz -C output .
-conda run -n sar_eddy_env aws s3 cp outputs.tar.gz s3://$OUTPUT_BUCKET_NAME/sar_eddy/${SAR_TASK_ID}_outputs.tar.gz
+conda run -n sar_eddy_env aws s3 cp outputs.tar.gz s3://$OUTPUT_BUCKET_NAME/sar_eddy/${SAR_TASK_ID}_outputs.tar.gz --region us-west-2
