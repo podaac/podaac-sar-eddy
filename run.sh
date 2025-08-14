490ac3c3507526bf1072880a30d8a2c9dab7f6b8
mike-gangl <michael.e.gangl@jpl.nasa.gov>
2025-08-14 13:04:03 -0700

    updated model to use

--- run.sh
+++ run.sh
@@ -20,7 +20,7 @@ cat config/hyp3/granules_batch.yaml
 
 
 #run intfernce only
-conda run -n sar_eddy_env python src/main.py hyp3=granules_batch #mode=inference_only paths.processed_dir=data/
+conda run -n sar_eddy_env python src/main.py hyp3=granules_batch inference=timm_xgb
 
 tar -czvf outputs.tar.gz -C output .
 conda run -n sar_eddy_env aws s3 cp outputs.tar.gz s3://$OUTPUT_BUCKET_NAME/sar_eddy/${SAR_TASK_ID}_outputs.tar.gz --region us-west-2
