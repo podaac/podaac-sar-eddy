7ff77e390e5bba1fc0656523411f7e10bea22e18
mike-gangl <michael.e.gangl@jpl.nasa.gov>
2025-08-14 08:43:33 -0700

    use new slice/sar code

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@ echo "$PWD"
 ls -l
 
 
-conda run -n sar_eddy_env aws configure list
-conda run -n sar_eddy_env python src/main.py --config config/inference.yaml
+#run intfernce only
+conda run -n sar_eddy_env python src/main.py mode=inference_only paths.processed_dir=data/
 
 tar -czvf outputs.tar.gz -C output .
 conda run -n sar_eddy_env aws s3 cp outputs.tar.gz s3://$OUTPUT_BUCKET_NAME/sar_eddy/${SAR_TASK_ID}_outputs.tar.gz --region us-west-2
