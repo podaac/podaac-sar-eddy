067cb32f71234b23049f0aa3b6fd5bdbb2c56de9
mike-gangl <michael.e.gangl@jpl.nasa.gov>
2025-09-26 12:42:53 -0700

    fixed usage of tarfile

--- run_inference.sh
+++ run_inference.sh
@@ -6,8 +6,7 @@ ls -l
 # filename like S1A_IW_GRDH_1SDV_20190101T142231_20190101T142256_025285_02CBF8_4954.tar.gz
 # Download the data/preview files
 aws s3 cp s3://$OUTPUT_BUCKET_NAME/sar_eddy/to_process/$FILE_NAME .
-tar -xvzf S1A_IW_GRDH_1SDV_20190101T142321_20190101T142346_025285_02CBF8_27F9.tar.gz .
-
+tar -xvzf $FILE_NAME .
 
 output_tar=$(./generate_name.sh $FILE_NAME)
 echo "$output_tar"
@@ -17,7 +16,6 @@ echo "$output_tar"
 conda run --no-capture-output -n sar_eddy_env python src/main.py mode=inference_only paths.processed_dir=data/processed  #hydra/job_logging=disabled #inference=timm_xgb
 
 
-
 #need to name the file appropriately here...
 tar -czvf $output_tar -C output .
-conda run -n sar_eddy_env aws s3 cp $output_tar s3://$OUTPUT_BUCKET_NAME/sar_eddy/${SAR_TASK_ID}_outputs.tar.gz --region us-west-2
+conda run -n sar_eddy_env aws s3 cp $output_tar s3://$OUTPUT_BUCKET_NAME/sar_eddy/$output_tar --region us-west-2
